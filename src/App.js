48b38cd330112b1b42e1a85743389826c57c06c7
Bramarika <bramarika.maddali@gmail.com>
2020-12-29 15:55:06 -0500

    Added onChange and onButtonClick Events

--- src/App.js
+++ src/App.js
@@ -17,19 +17,38 @@ const particlesOptions = {
     }
   }
 }
-function App() {
-  return (
-    <div className="App">
-    <Particles className='particles'
-      params={particlesOptions}
-    />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+  render(){
+    return (
+      <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm  
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
