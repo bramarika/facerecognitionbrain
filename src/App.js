7c43105b878b293dcdf4265fe64bf3b507617f1c
Bramarika <bramarika.maddali@gmail.com>
2020-12-29 14:45:45 -0500

    Added Rank Component, changed the CSS for the form and added Particles background from react-particles-js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
-
+const particlesOptions = {
+  particles: {
+    number:{
+        value: 5,
+        density:{
+          enable:true,
+          value_area: 20
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
+    <Particles className='particles'
+      params={particlesOptions}
+    />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
