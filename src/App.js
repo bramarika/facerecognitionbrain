7a8cb065b21f9789f9c455f3524192e0b321a754
Bramarika <bramarika.maddali@gmail.com>
2020-12-31 20:43:31 -0500

    Added functionality to route to different pages, and to show the square box around the image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import './App.css';
 
@@ -27,32 +29,44 @@ const particlesOptions = {
   }
 }
 
-
 class App extends Component {
   constructor(){
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box:{},
+      route: 'signIn',
+      isSignedIn: false
     }
+  }
+  calculateFaceLocation = (data) => { 
+    const clarifaiFaceBox = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFaceBox.left_col * width,
+      topRow: clarifaiFaceBox.top_row * height,
+      rightCol: width - (clarifaiFaceBox.right_col * width),
+      bottomRow : height - (clarifaiFaceBox.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    /*app.models
-      .predict(Clarifai.COLOR_MODEL,"https://samples.clarifai.com/face-det.jpg")
-      .then(
-        function(response){
-          console.log(response);
-        },
-        function(err){
-
-        }
-      );
-    }*/
-    app.models.initModel({id: Clarifai.COLOR_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err));
+    }
+    /*app.models.initModel({id: Clarifai.COLOR_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
       .then(generalModel => {
         return generalModel.predict(this.state.input);
       })
@@ -60,22 +74,41 @@ class App extends Component {
         var concepts = response['outputs'][0]['data']['concepts']
         console.log(concepts);
     });
-    }
+    }*/
   
+onRouteChange = (route) =>{
+  if(route === 'signout'){
+    this.setState({isSignedIn : false})
+  } else if(route === 'home'){
+    this.setState({isSignedIn : true})
+  }
+  this.setState({route : route})
+}
+
   render(){
+    const {isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-      <Particles className='particles'
-        params={particlesOptions}
-      />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm  
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}
+        <Particles className='particles'
+          params = {particlesOptions}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange = {this.onRouteChange}/>
+        {route === 'home'
+        ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm  
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+          : (
+          route === 'signIn'
+          ? <SignIn onRouteChange = {this.onRouteChange}/>
+            : <Register onRouteChange = {this.onRouteChange}/>
+          )
+      }
       </div>
     );
   }
